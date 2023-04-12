e5eb287a3061cb16cbf3c23b853e887c713fb2e4
Koyoinu <85961107+Koyoinu@users.noreply.github.com>
2023-04-12 08:29:49 -0500

    Welcomemed coded

--- code.js
+++ code.js
@@ -7,6 +7,15 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
+client.on("guildMemberAdd", (member) => {
+    const welcomeEmbed = new Discord.MessageEmbed()
+        .setTitle('**Welcome to Lewd Universe!**')
+        .setDescription('Please get comfy and feel free to look around after you have done all the required stuff after joining the server!~')
+        .setColor('#9900ff')
+        .setFooter('Lewd Welcomer V1 | 2023')
+        .setTimestamp();
+    const channle = member.guild.channels.cache.get(Mainchan);
+    channelLink.send(welcomeEmbed);
+})
 
 client.login('ID');
