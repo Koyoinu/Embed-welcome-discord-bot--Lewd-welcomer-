394d1fcfcec50d6242fe831cad3c44c0c69958f6
Koyoinu <85961107+Koyoinu@users.noreply.github.com>
2023-04-13 01:36:47 -0500

    discord new API bullshit

--- code.js
+++ code.js
@@ -1,5 +1,14 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
 const ID = "";
 const Mainchan = "";
 
